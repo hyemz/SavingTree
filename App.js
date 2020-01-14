d7aa7f9db8ecdf5a6f121bc6286ae1b06f1e8c13
sh512 <42368071+sh512@users.noreply.github.com>
2020-01-14 14:29:59 +0900

    re이름 변경

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app! ¹Ú¼­Èñ</Text>
+      <Text>Open up App.js to start working on your app! ë°•ì„œí¬</Text>
     </View>
   );
 }
